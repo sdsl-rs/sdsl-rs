a6c5b01bd7e84a79e43177e5f8e5176d660825c7
rndhouse <rndhouse@protonmail.com>
2021-05-20 18:28:17 +0100

    Add get_int and get_bv_element tests.

--- examples/src/rrr_vector.rs
+++ examples/src/rrr_vector.rs
@@ -36,3 +36,25 @@ fn test_from_file() -> Result<()> {
     assert_eq!(result, expected);
     Ok(())
 }
+
+#[test]
+fn test_get_int() -> Result<()> {
+    let bv = sdsl::bit_vector! {1, 1, 0, 1};
+    let rv = sdsl::RrrVector::<sdsl::IntVector<0>, 10, 2>::new(&bv)?;
+
+    let result = rv.get_int(1, 3);
+    let expected = 5;
+    assert_eq!(result, expected);
+    Ok(())
+}
+
+#[test]
+fn test_get_bv_element() -> Result<()> {
+    let bv = sdsl::bit_vector! {1, 1, 0, 1};
+    let rv = sdsl::RrrVector::<sdsl::IntVector<0>, 10, 2>::new(&bv)?;
+
+    let result = rv.get_bv_element(2);
+    let expected = 0;
+    assert_eq!(result, expected);
+    Ok(())
+}
