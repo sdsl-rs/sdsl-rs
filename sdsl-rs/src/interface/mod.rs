52b307037a246335a95402566dab8ae067251802
rndhouse <rndhouse@protonmail.com>
2021-06-18 10:47:17 +0100

    Move crate interface definition to lower stack module.

--- sdsl-rs/src/interface/mod.rs
+++ sdsl-rs/src/interface/mod.rs
@@ -1,8 +1,31 @@
-pub mod bit_vector;
-pub mod byte_tree;
-pub mod common;
-pub mod int_vector;
-pub mod rank_support_v;
-pub mod rrr_vector;
-pub mod select_support_mcl;
-pub mod wt_huff;
+mod bit_vector;
+mod common;
+mod int_vector;
+mod rank_support_v;
+mod rrr_vector;
+mod select_support_mcl;
+
+pub mod crate_export {
+    pub use crate::interface::common::io::crate_export as io;
+    pub use crate::interface::common::util::crate_export as util;
+
+    pub mod bit_patterns {
+        pub use crate::interface::common::bit_patterns::{P0, P01, P1, P10};
+    }
+
+    pub mod int_vectors {
+        pub use crate::interface::int_vector::IntVector;
+    }
+
+    pub mod bit_vectors {
+        pub use crate::interface::{bit_vector::BitVector, rrr_vector::RrrVector};
+    }
+
+    pub mod rank_supports {
+        pub use crate::interface::rank_support_v::RankSupportV;
+    }
+
+    pub mod select_supports {
+        pub use crate::interface::select_support_mcl::SelectSupportMcl;
+    }
+}
--- sdsl-rs/src/lib.rs
+++ sdsl-rs/src/lib.rs
@@ -5,29 +5,4 @@ mod interface;
 mod meta;
 
 pub use crate::backend::build;
-pub use crate::interface::common::io::crate_export as io;
-pub use crate::interface::common::util::crate_export as util;
-
-pub mod bit_patterns {
-    pub use crate::interface::common::bit_patterns::{P0, P01, P1, P10};
-}
-
-pub mod int_vectors {
-    pub use crate::interface::int_vector::IntVector;
-}
-
-pub mod bit_vectors {
-    pub use crate::interface::{bit_vector::BitVector, rrr_vector::RrrVector};
-}
-
-pub mod rank_supports {
-    pub use crate::interface::rank_support_v::RankSupportV;
-}
-
-pub mod select_supports {
-    pub use crate::interface::select_support_mcl::SelectSupportMcl;
-}
-
-pub mod wavelet_trees {
-    pub use crate::interface::{byte_tree::ByteTree, wt_huff::WtHuff};
-}
+pub use crate::interface::crate_export::*;
