811e93c0c791d612379a165abb59e7e22773b0c4
rndhouse <rndhouse@protonmail.com>
2022-01-23 00:35:36 +0000

    sdsl c template version++

--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
-VERSION="v0.1.29"
+VERSION="v0.2.0"
 TARGET=./sdsl-rs/src/backend/sdsl_c/sdsl-c-template.zip
 if [ ! -f "$TARGET" ]; then
     wget -O $TARGET https://github.com/sdsl-rs/sdsl-c-template/releases/download/${VERSION}/sdsl-c-template-${VERSION}.zip
